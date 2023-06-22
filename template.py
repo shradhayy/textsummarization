3f2c9b31fe00ffe8e2fde3fb9a2d9e72653d6fe6
Shradhay <aryanshradhay@outlook.com>
2023-06-23 04:15:34 +0530

    files and project structure added

--- template.py
+++ template.py
@@ -4,15 +4,14 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
+project_name="textsummarizer"
 
-project_name = "TEXTSUMMARIZATION"
-
-list_of_files = [
+list_files=[
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
-    f"src/{project_name}/conponents/__init__.py",
-    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/logging/__init__.py",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
@@ -24,28 +23,30 @@ list_of_files = [
     "app.py",
     "main.py",
     "Dockerfile",
-    "requirements.txt",
+    "requiements.txt",
     "setup.py",
-    "research/trials.ipynb",
-
+    "research/trials.ipynb"
 ]
 
+for filepath in list_files:
+    filepath=Path(filepath)
+    filedir,filename=os.path.split(filepath)
 
-for filepath in list_of_files:
-    filepath = Path(filepath)
-    filedir, filename = os.path.split(filepath)
+    if filedir!="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"creating directory:{filedir} for the file {filename}")
 
-    if filedir != "":
-        os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory:{filedir} for the file {filename}")
-
-    
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath,'w') as f:
             pass
-            logging.info(f"Creating empty file: {filepath}")
+            logging.info(f"creating empty file:{filepath}")
+
+    else :
+        logging.info(f"file already exists:{filepath}")
 
 
-    
-    else:
-        logging.info(f"{filename} is already exists")
+        
+
+
+
+
